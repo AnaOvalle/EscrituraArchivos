fa743b8266154a48927aaaaf4b1160e8a0f52089
Ana Ovalle <l21212014@tectijuana.edu.mx>
2022-05-31 11:25:11 -0700

    version de clase
    trabajo de clase

---
+++ EscrituraArchivos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            // si el archivo no existe creará el elemento o archivo
+            // si ya existe escribirá en el.
+            // true es para agregar y no para sobrescribir
+
+            // crear un arreglo 
+            string[] lines =
+            {
+                "Esta es la información de la primera línea",
+                "Esta es la segunda línea",
+                "fin del texto"
+            };
+
+            // Recorre el arreglo
+            foreach  (string line in lines)
+            {
+                sw.WriteLine(line); //Escribe el archivo
+            }
+            sw.Close();
+
+            Console.WriteLine("Escribiendo el archivo...");
+            Console.ReadLine();
+                
+        }
+    }
+}
